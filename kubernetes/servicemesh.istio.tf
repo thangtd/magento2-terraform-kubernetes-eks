b5c3d7100b48214186f3c177c2add88f90ce4342
Henry Tran <thangtd@gmail.com>
2022-07-25 15:30:16 +0700

    Add service mesh istio

--- kubernetes/servicemesh.istio.tf
+++ kubernetes/servicemesh.istio.tf
@@ -31,6 +31,7 @@ resource "helm_release" "istio_base" {
   name       = "istio-base"
   repository = "https://istio-release.storage.googleapis.com/charts"
   chart      = "base"
+  version    = "1.14.2"
 
   namespace       = kubernetes_namespace.istio_system.metadata.0.name
   cleanup_on_fail = true
@@ -42,6 +43,7 @@ resource "helm_release" "istio_istiod" {
   name       = "istio-istiod"
   repository = "https://istio-release.storage.googleapis.com/charts"
   chart      = "istiod"
+  version    = "1.14.2"
 
   namespace       = kubernetes_namespace.istio_system.metadata.0.name
   cleanup_on_fail = true
@@ -51,15 +53,31 @@ resource "helm_release" "istio_istiod" {
 
 }
 
-# resource "helm_release" "istio_ingress_gateway" {
-#   name       = "istio-ingress-gateway"
-#   repository = "https://istio-release.storage.googleapis.com/charts"
-#   chart      = "gateway"
+resource "helm_release" "istio_ingress_gateway" {
+  name       = "istio-ingress-gateway"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "gateway"
+  version   =  "1.14.2"
 
-#   namespace       = kubernetes_namespace.istio_ingress.metadata.0.name
-#   cleanup_on_fail = true
-#   force_update    = false
+  namespace       = kubernetes_namespace.istio_ingress.metadata.0.name
+  cleanup_on_fail = true
+  force_update    = false
 
-#   depends_on = [helm_release.istio_istiod]
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
 
-# }
+  set {
+    name  = "labels.app"
+    value = "istio-ingressgateway"
+  }
+
+  set {
+    name  = "labels.istio"
+    value = "ingressgateway" # This is kind of a selector for the gateway.
+  }
+
+  depends_on = [helm_release.istio_istiod]
+
+}
