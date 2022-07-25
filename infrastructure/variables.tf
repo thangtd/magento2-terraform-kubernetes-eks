ee7da3fa079dc57cb6092b1eedfffbd437e56bfd
Henry Tran <thangtd@gmail.com>
2022-07-25 12:23:31 +0700

    Change instance type of node group

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -57,17 +57,20 @@ variable "ng_ssh_key" {
 
 variable "amitype" {
   type    = string
-  default = "AL2_ARM_64"
-  # default = "AL2_x86_64"
+  # default = "AL2_ARM_64"
+  
+  default = "AL2_x86_64"
   # Valid values: AL2_x86_64 | AL2_x86_64_GPU | AL2_ARM_64 | CUSTOM | BOTTLEROCKET_ARM_64 | BOTTLEROCKET_x86_64
   # Need to build the docker image differently https://gist.github.com/foo4u/84926426bb9f56166cde4e40efc37b5e
 }
 
 variable "instance_types" {
   type = set(string)
-  # memory intensive work load
-  default = ["t4g.medium","m6g.medium","r6g.medium", "r6gd.medium", "t4g.large"]
-  # default = ["t3.medium"]
+  # ARM architecture
+  # default = ["t4g.medium","m6g.medium","r6g.medium", "r6gd.medium", "t4g.large"]
+
+  # x86 architecture
+  default = ["t3a.medium","t3.medium","t2.medium"]
 }
 
 variable "capacity_type" {
