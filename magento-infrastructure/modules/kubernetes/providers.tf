6d6f46379223134b011ac3666d4f4de84b63edba
Henry Tran <thangtd@gmail.com>
2022-08-01 14:59:29 +0700

    Terraform formatting

--- magento-infrastructure/modules/kubernetes/providers.tf
+++ magento-infrastructure/modules/kubernetes/providers.tf
@@ -15,7 +15,7 @@ terraform {
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.7.0"
-    }    
+    }
   }
 }
 
@@ -92,6 +92,6 @@ provider "helm" {
 provider "kubectl" {
   host                   = data.aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token  
+  token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
   load_config_file       = false
 }
