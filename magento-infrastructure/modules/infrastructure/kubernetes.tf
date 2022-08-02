da3042cb86628d675756aef4a5234b65946dfda5
Henry Tran <thangtd@gmail.com>
2022-08-02 22:05:01 +0700

    Terraform Best Practices

--- magento-infrastructure/modules/infrastructure/kubernetes.tf
+++ magento-infrastructure/modules/infrastructure/kubernetes.tf
@@ -3,13 +3,44 @@
 ################################################################################
 
 data "tls_certificate" "eks_tls_cert" {
-  url        = aws_eks_cluster.magento_eks.identity.0.oidc.0.issuer
-  depends_on = [aws_eks_node_group.eks_node_group]
+  url = aws_eks_cluster.magento_eks.identity.0.oidc.0.issuer
+  depends_on = [
+    aws_eks_node_group.eks_node_group,
+    aws_eks_fargate_profile.magento_fargate_profile
+  ]
 }
 
 resource "aws_iam_openid_connect_provider" "eks_identity_provider" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.eks_tls_cert.certificates.0.sha1_fingerprint]
   url             = aws_eks_cluster.magento_eks.identity.0.oidc.0.issuer
-  depends_on      = [aws_eks_node_group.eks_node_group]
+  depends_on = [
+    aws_eks_node_group.eks_node_group,
+    aws_eks_fargate_profile.magento_fargate_profile
+  ]
+}
+
+
+################################################################################
+# MONITORING: METRICS SERVER - PROMETHEUS - GRAFANA
+################################################################################
+resource "kubernetes_namespace_v1" "monitoring_namespace" {
+  metadata {
+    name = "monitoring"
+  }
+
+  depends_on = [
+    aws_iam_openid_connect_provider.eks_identity_provider
+  ]
+}
+
+resource "helm_release" "metrics_server" {
+
+  name            = "metrics-server"
+  repository      = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart           = "metrics-server"
+  namespace       = kubernetes_namespace_v1.monitoring_namespace.id
+  cleanup_on_fail = true
+  force_update    = false
+
 }
--- magento-infrastructure/modules/infrastructure/main.tf
+++ magento-infrastructure/modules/infrastructure/main.tf
@@ -39,34 +39,6 @@ resource "aws_vpc" "magento_vpc" {
 
 }
 
-resource "aws_internet_gateway" "magento_igw" {
-  vpc_id = aws_vpc.magento_vpc.id
-
-  tags = {
-    Name = "${local.name}-igw"
-  }
-}
-
-resource "aws_eip" "magento_eip_for_natgw" {
-  vpc = true
-}
-
-resource "aws_nat_gateway" "magento_nat_gateway" {
-  allocation_id = aws_eip.magento_eip_for_natgw.id
-  subnet_id     = aws_subnet.magento_public_subnet_1.id
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "${local.name}-nat-gateway"
-    }
-  )
-
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.magento_igw]
-}
-
 resource "aws_subnet" "magento_public_subnet_1" {
   vpc_id     = aws_vpc.magento_vpc.id
   cidr_block = "10.0.0.0/24"
@@ -102,7 +74,8 @@ resource "aws_subnet" "magento_private_subnet_1" {
   availability_zone = data.aws_availability_zones.az.names[0]
 
   tags = {
-    Name                                          = "${local.name}-private-subnet-1"
+    Name = "${local.name}-private-subnet-1"
+    # AWS Ingress Controller needs these tags to identify the subnet
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = 1
   }
@@ -129,8 +102,6 @@ resource "aws_route_table" "magento_public_route_table" {
   }
 }
 
-# add a new route to the route table
-
 resource "aws_route" "magento_public_route" {
   route_table_id         = aws_route_table.magento_public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
@@ -148,16 +119,6 @@ resource "aws_route_table_association" "public_subnet2_to_public_rt" {
   route_table_id = aws_route_table.magento_public_route_table.id
 }
 
-resource "aws_route_table_association" "private_subnet1_to_private_rt" {
-  subnet_id      = aws_subnet.magento_private_subnet_1.id
-  route_table_id = aws_route_table.magento_private_route_table.id
-}
-
-resource "aws_route_table_association" "private_subnet2_to_private_rt" {
-  subnet_id      = aws_subnet.magento_private_subnet_2.id
-  route_table_id = aws_route_table.magento_private_route_table.id
-}
-
 resource "aws_route_table" "magento_private_route_table" {
   vpc_id = aws_vpc.magento_vpc.id
 
@@ -172,9 +133,76 @@ resource "aws_route" "magento_private_route" {
   nat_gateway_id         = aws_nat_gateway.magento_nat_gateway.id
 }
 
+resource "aws_route_table_association" "private_subnet1_to_private_rt" {
+  subnet_id      = aws_subnet.magento_private_subnet_1.id
+  route_table_id = aws_route_table.magento_private_route_table.id
+}
+
+resource "aws_route_table_association" "private_subnet2_to_private_rt" {
+  subnet_id      = aws_subnet.magento_private_subnet_2.id
+  route_table_id = aws_route_table.magento_private_route_table.id
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "magento_igw" {
+  vpc_id = aws_vpc.magento_vpc.id
+
+  tags = {
+    Name = "${local.name}-igw"
+  }
+}
+
+
+# NAT Gateway
+resource "aws_eip" "magento_eip_for_natgw" {
+  vpc  = true
+  tags = local.common_tags
+}
+
+resource "aws_nat_gateway" "magento_nat_gateway" {
+  allocation_id = aws_eip.magento_eip_for_natgw.id
+  subnet_id     = aws_subnet.magento_public_subnet_1.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.name}-nat-gateway"
+    }
+  )
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.magento_igw]
+}
+
 ################################################################################
 # EKS Cluster
 ################################################################################
+
+resource "aws_iam_role" "eks_iam_role" {
+  name = "${local.cluster_name}-iam-role"
+
+  assume_role_policy = <<POLICY
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Principal": {
+            "Service": "eks.amazonaws.com"
+          },
+          "Action": "sts:AssumeRole"
+        }
+      ]
+    }
+    POLICY
+
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_iam_role.name
+}
 
 resource "aws_eks_cluster" "magento_eks" {
   name     = local.cluster_name
@@ -188,92 +216,30 @@ resource "aws_eks_cluster" "magento_eks" {
     subnet_ids = [aws_subnet.magento_private_subnet_1.id, aws_subnet.magento_private_subnet_2.id]
   }
 
+  tags = local.common_tags
+
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy_attachment,
-    aws_vpc.magento_vpc
-  ]
-
-  tags = local.common_tags
-}
-
-resource "aws_iam_role" "eks_iam_role" {
-  name = "${local.cluster_name}-iam-role"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
+    aws_vpc.magento_vpc,
+    aws_subnet.magento_private_subnet_1,
+    aws_subnet.magento_private_subnet_2,
+    aws_subnet.magento_public_subnet_1,
+    aws_subnet.magento_public_subnet_2,
+    aws_route.magento_public_route,
+    aws_route.magento_private_route,
+    aws_route_table_association.public_subnet1_to_public_rt,
+    aws_route_table_association.public_subnet2_to_public_rt,
+    aws_route_table_association.private_subnet1_to_private_rt,
+    aws_route_table_association.private_subnet2_to_private_rt,
+    aws_nat_gateway.magento_nat_gateway,
+    aws_iam_role_policy_attachment.eks_cluster_policy_attachment
   ]
 }
-POLICY
-
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_iam_role.name
-}
-
-
 
 ################################################################################
 # EKS Node Group
 ################################################################################
-
-resource "aws_eks_node_group" "eks_node_group" {
-  cluster_name    = aws_eks_cluster.magento_eks.name
-  node_group_name = "${local.cluster_name}-memory-ng"
-  node_role_arn   = aws_iam_role.eks_ng_iam_role.arn
-  #subnet_ids      = [aws_subnet.magento_private_subnet_1.id, aws_subnet.magento_private_subnet_2.id]
-  subnet_ids = [aws_subnet.magento_public_subnet_1.id, aws_subnet.magento_public_subnet_2.id]
-
-  ami_type       = var.amitype
-  instance_types = var.instance_types
-  capacity_type  = var.capacity_type
-
-  remote_access {
-    ec2_ssh_key = var.ng_ssh_key
-    #source_security_group_ids = [aws_security_group.magento_bastion_sg.id]
-  }
-
-  scaling_config {
-    desired_size = var.ng_desired_size
-    max_size     = var.ng_max_size
-    min_size     = var.ng_min_size
-  }
-
-  update_config {
-    max_unavailable = 1
-  }
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  depends_on = [
-    aws_iam_role_policy_attachment.eks-ng-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.eks-ng-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.eks-ng-AmazonEC2ContainerRegistryReadOnly,
-    aws_vpc.magento_vpc,
-    aws_eks_cluster.magento_eks
-  ]
-
-  tags = merge(
-    local.common_tags,
-    {
-      "k8s.io/cluster-autoscaler/enabled"               = true
-      "k8s.io/cluster-autoscaler/${local.cluster_name}" = "owned"
-    }
-  )
-  labels = local.common_tags
-}
 
 resource "aws_iam_role" "eks_ng_iam_role" {
   name = "${local.cluster_name}-ng-role"
@@ -304,6 +270,57 @@ resource "aws_iam_role_policy_attachment" "eks-ng-AmazonEC2ContainerRegistryRead
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_ng_iam_role.name
 }
+
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.magento_eks.name
+  node_group_name = "${local.cluster_name}-memory-ng"
+  node_role_arn   = aws_iam_role.eks_ng_iam_role.arn
+  #subnet_ids      = [aws_subnet.magento_private_subnet_1.id, aws_subnet.magento_private_subnet_2.id]
+
+  # TODO: Using public subnets for testing purpose using NodePort
+  subnet_ids = [aws_subnet.magento_public_subnet_1.id, aws_subnet.magento_public_subnet_2.id]
+
+  ami_type       = var.amitype
+  instance_types = var.instance_types
+  capacity_type  = var.capacity_type
+
+  remote_access {
+    ec2_ssh_key = var.ng_ssh_key
+    #source_security_group_ids = [aws_security_group.magento_bastion_sg.id]
+  }
+
+  scaling_config {
+    desired_size = var.ng_desired_size
+    max_size     = var.ng_max_size
+    min_size     = var.ng_min_size
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      # Cluster Autoscaler needs these tags to discover the node group.
+      "k8s.io/cluster-autoscaler/enabled"               = true
+      "k8s.io/cluster-autoscaler/${local.cluster_name}" = "owned"
+    }
+  )
+
+  labels = local.common_tags
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-ng-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks-ng-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks-ng-AmazonEC2ContainerRegistryReadOnly,
+    aws_eks_cluster.magento_eks
+  ]
+
+}
+
 
 ################################################################################
 # EKS Fargate Profile
@@ -392,5 +409,10 @@ resource "aws_eks_fargate_profile" "magento_fargate_profile" {
       tier  = "worker"
     }
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.magento_fargate_role_policy_attachment,
+    aws_eks_cluster.magento_eks
+  ]
 
 }
